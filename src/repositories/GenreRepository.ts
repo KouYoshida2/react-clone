aa2f89ffeae4bd70cdc24f47f6f154e426b4b1d9
k.yoshida <>
2023-01-12 20:36:27 +0900

    feat:ジャンル一覧を取得・表示

--- src/repositories/GenreRepository.ts
+++ src/repositories/GenreRepository.ts
@@ -1,12 +1,13 @@
 import repository from "./client/apiClient";
+const key = import.meta.env.VITE_API_KEY;
 export const useGenreRepository = (): GenreRepository => {
   /**
    * - ジャンルの一覧を取得する
    * @returns
    */
   const getGenreList = async () => {
-    const { data } = await repository.get("/genre/movie/list");
-    return data;
+    const { data } = await repository.get(`/genre/movie/list?api_key=${key}`);
+    return data.genres;
   };
 
   return { getGenreList };
