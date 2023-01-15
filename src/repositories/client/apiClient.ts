0e1947d204189a3f4457dccaefdbdba658494cf4
k.yoshida <>
2023-01-15 16:34:49 +0900

    feat:MSWを導入、モックAPIを作成、react-router-domをインストール

--- src/repositories/client/apiClient.ts
+++ src/repositories/client/apiClient.ts
@@ -1,4 +1,12 @@
 import axios, { AxiosInstance } from "axios";
+import { worker } from "../../mocks/browser";
+
+if (import.meta.env.DEV) {
+  worker.start({
+    // モックしていない通信のwarningを消す
+    onUnhandledRequest: "bypass",
+  });
+}
 
 const repository: AxiosInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
